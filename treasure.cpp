a14f57aa34260a23e5411eefe83ff005cdadc3c7
M Faizan <f2024376210@umt.edu.pk>
2025-05-08 15:46:22 -0700

    new

--- treasure.cpp
+++ treasure.cpp
@@ -4,7 +4,7 @@
 #include<ctime>
 #include<cstdlib>
 using namespace std;
-class game{
+class gam{
     public:
     int row;
     int col;
@@ -13,7 +13,7 @@ class game{
     int p_r;
     int p_c;
     vector<vector<char>>v;
-    game(int a,int b):row(a),col(b),v(a,vector<char>(b,'*')),p_r(0),p_c(0){}
+    gam(int a,int b):row(a),col(b),v(a,vector<char>(b,'*')),p_r(0),p_c(0){}
     void mod();
     void dis();
     void tre();
@@ -35,7 +35,7 @@ class game{
       tr=arr[2];
     }
 };
-void game::mod()
+void gam::mod()
 {
     int arr[3]={4,6,5};
     vector<string>e={"Easy","Medium","Hard"};
@@ -61,7 +61,7 @@ void game::mod()
         cout<<"you are not entering correct::"<<endl;
     }
 }
-void game::dis()
+void gam::dis()
 {
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
@@ -76,7 +76,7 @@ void game::dis()
         cout << endl;
     }
 }
-void game::tre()
+void gam::tre()
 {
     int e=0;
     while(e<t)
@@ -91,7 +91,7 @@ void game::tre()
 }
 
     }
-    void game::trap()
+    void gam::trap()
     {
         int tra_c=0;
     while(tra_c<tr)
@@ -106,7 +106,7 @@ void game::tre()
     }
     
 }
-void game::key()
+void gam::key()
 {
     char se;
     cout<<"Enter the position you want to move ::::w|up:::s|down:::a|left:::d|right:::"<<endl;
@@ -141,21 +141,21 @@ int main()
     cin>>r;
     if(r>10)
     {
-        cout<<"not in limit enter agamein::"<<endl;
+        cout<<"not in limit enter agamin::"<<endl;
         cin>>r;
     }
     cout<<"now enter the column limit 5-10::"<<endl;
     cin>>c;
     if(c>10)
     {
-        cout<<"not in limit enter agamein::"<<endl;
+        cout<<"not in limit enter agamin::"<<endl;
         cin>>c;
     }
- vector<game>g1;
- g1.push_back(game(r,c));
- cout<<"now choose the gamee Mod::"<<endl;
+ vector<gam>g1;
+ g1.push_back(gam(r,c));
+ cout<<"now choose the game Mod::"<<endl;
  g1[0].mod();
- cout<<"Press Enter to start of the gamee::"<<endl;
+ cout<<"Press Enter to start of the game::"<<endl;
  getch();
 g1[0].dis();
 g1[0].trap();
